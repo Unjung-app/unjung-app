a5ef76181739c3957d03bbf680bf9413391d580b
25055 <25055@unjung.hs.kr>
2026-08-28 16:25:46 +0900

    ! 추가

--- app/study/page.js
+++ app/study/page.js
@@ -8,7 +8,7 @@ export default function StudyPage() {
   return (
     <ComingSoon
       title="전학년 공부시간 경쟁"
-      desc="공부시간 기록과 학년별 순위를 보여줄 화면입니다. 곧 만나요!"
+      desc="공부시간 기록과 학년별 순위를 보여줄 화면입니다. 곧 만나요!!!"
       icon="timer"
     />
   );
